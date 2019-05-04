131208330649e57610bb1fbba4818625420a4e61
hassanahmed95 <mr.hassanahmed95@gmail.com>
2019-05-04 16:09:19 +0500

    the method to get the mean length of the voice signals has been calculated

--- mean_distance_calculation.py
+++ mean_distance_calculation.py
@@ -1,2 +1,20 @@
-print ("Hello word . . .")
-print("here is my new folr")
+import os
+import soundfile as sf
+import numpy as np
+
+root_path = "/home/hassan/Hassaan_Home/Digityfy_Projects/SER_Tech_Stuff/berlin_dataset"
+results = []
+
+all_folders = os.listdir(root_path)
+
+for i in all_folders:
+    current_folder = root_path + "/" + i
+    os.chdir(current_folder)
+    data = os.listdir(current_folder)
+    for wav_file in data:
+        signal, fs = sf.read(wav_file)
+        results.append(len(signal))
+
+
+results = np.array(results)
+print(np.mean(results).round(2))

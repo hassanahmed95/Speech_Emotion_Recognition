7efe035fd8b7311cc82926393c12fdee65555160
hassanahmed95 <mr.hassanahmed95@gmail.com>
2019-05-06 15:01:52 +0500

    here i am performing the various tests, to make it dynamic

---
+++ user_inteface.py
@@ -0,0 +1,88 @@
+
+import tkinter as tk
+from tkinter import filedialog
+root = tk.Tk()
+import numpy as np
+import scipy.io.wavfile as wav
+from speechpy.feature import mfcc
+import soundfile as sf
+import os
+import pickle
+
+mean_signal_length = 45000
+
+
+def on_click():
+    file1 = filedialog.askopenfilename()
+    print(file1)
+    # label1 = tk.Label(text = file1).pack()
+    return file1
+
+
+def min_max_scalling(data):
+    my_list = []
+    for value in data:
+        my_list.append((value - np.min(data)) /
+                       (np.max(data)-np.min(data)))
+
+    return np.array(my_list)
+
+def get_feature_vector_from_mfcc(file_path: str, mfcc_len: int =45 ):
+    # sf, signal = wav.read(file_path)
+    signal, fs = sf.read(file_path)
+    s_len = len(signal)
+    # print(s_len)
+
+    if s_len < mean_signal_length:
+        # print("I am in the if loop, for the features padding")
+        pad_len = mean_signal_length - s_len
+        pad_len //= 2
+        pad_rem = pad_len % 2
+        signal = np.pad(signal, (pad_len, pad_len + pad_rem),
+                        'constant', constant_values=0)
+
+    else:
+        print("I am in the else loop")
+        pad_len = s_len - mean_signal_length
+        pad_len //= 2
+
+        signal = signal[pad_len:pad_len + mean_signal_length]
+
+
+    mel_coefficients = mfcc(signal, fs, num_cepstral=mfcc_len)
+
+    mel_coefficients = np.ravel(mel_coefficients)
+
+
+    normalize_feature_vector= min_max_scalling(mel_coefficients)
+
+    return normalize_feature_vector
+
+
+def testing():
+    print("Now i am in the testing method .  . . .")
+
+    pickle_in = open("/home/hassan/Hassaan_Home/My_Python_Projects/Speech_Project/My_ML_Models/SVM_Model.pickle", "rb")
+    model = pickle.load(pickle_in)
+
+    file_path = on_click()
+    file = "/home/hassan/Hassaan_Home/Digityfy_Projects/SER_Tech_Stuff/Audio_Speech_Actors_01-24/Actor_01/03-01-01-01-01-01-01.wav"
+    data = get_feature_vector_from_mfcc(file_path)
+
+    prediction = model.predict([data])[0]
+
+    if prediction == 0:
+        print("The prediction is" + " Angry")
+    elif prediction ==1:
+        print("The prediction is" + " happy")
+    elif prediction ==2:
+        print("The prediction is" + " Neutral")
+    else :
+        print("The prediction is" + " Sad")
+
+button = tk.Button(root, text ="Open File ", width =30, command=on_click).pack()
+root.title("Hello that is my title of the window . . .")
+root.geometry("400x400")
+root.mainloop()
+testing()
+

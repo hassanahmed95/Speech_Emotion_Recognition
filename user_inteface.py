abb6796049b7acc369b2fd6b1a50e37df73ebcf4
hassanahmed95 <mr.hassanahmed95@gmail.com>
2019-05-07 12:09:51 +0500

    The user interface has been added in the project, now the user can upload an aaudio file via GUI to check the emotion in the audio file. . .

--- user_inteface.py
+++ user_inteface.py
@@ -1,47 +1,13 @@
-
-import tkinter as tk
+import tkinter
+from tkinter import *
 from tkinter import filedialog
-
 import numpy as np
-import scipy.io.wavfile as wav
-from speechpy.feature import mfcc
 import soundfile as sf
-import os
+from speechpy.feature import mfcc
 import pickle
-# from model_testing1 import testing
 
-mean_signal_length = 45000
+mean_signal_length =  45000
 
-root = tk.Tk()
-
-
-def quit():
-    root.quit()
-
-def on_click():
-    file1 = filedialog.askopenfilename()
-    print(file1)
-    # print(type(file1))
-    # exit()
-    data =get_feature_vector_from_mfcc(file1)
-
-    pickle_in = open("/home/hassan/Hassaan_Home/My_Python_Projects/Speech_Project/My_ML_Models/SVM_Model.pickle", "rb")
-    model = pickle.load(pickle_in)
-
-    prediction = model.predict([data])[0]
-
-    if prediction == 0:
-        print("The prediction is" + " Angry")
-    elif prediction ==1:
-        print("The prediction is" + " happy")
-    elif prediction ==2:
-        print("The prediction is" + " Neutral")
-    else :
-        print("The prediction is" + " Sad")
-
-
-    label1 = tk.Label(text = prediction).pack()
-    # return file1
 
 def min_max_scalling(data):
     my_list = []
@@ -56,6 +22,7 @@ def get_feature_vector_from_mfcc(file_path: str, mfcc_len: int =45 ):
     # sf, signal = wav.read(file_path)
     signal, fs = sf.read(file_path)
     s_len = len(signal)
+    fs = 16000
     # print(s_len)
 
     if s_len < mean_signal_length:
@@ -73,22 +40,74 @@ def get_feature_vector_from_mfcc(file_path: str, mfcc_len: int =45 ):
 
         signal = signal[pad_len:pad_len + mean_signal_length]
 
-
     mel_coefficients = mfcc(signal, fs, num_cepstral=mfcc_len)
-
     mel_coefficients = np.ravel(mel_coefficients)
-
-
     normalize_feature_vector= min_max_scalling(mel_coefficients)
 
     return normalize_feature_vector
 
-if __name__ == "__main__":
-    # while(True):
-        button = tk.Button(root, text ="Open File ", width =30, command=on_click).pack()
-        button2 = tk.Button(root, text="Quit ", width=30, command = quit).pack()
-        root.title("Hello that is my title of the window . . .")
-        root.geometry("400x400")
-        root.mainloop()
 
 
+def main_interface():
+    #it is main interface and it has been declared as global, so that
+    #it can accessed throughout the file
+
+    global master
+    master = Tk()
+    master.geometry("500x300")
+    master.title("Speech Emotion Recognition . . .")
+
+    my_text = "Speech Emotion Recognition Portal"
+    msg = Message(master, text=my_text)
+    msg.config(justify=CENTER, font=('times', 25, 'italic'))
+    msg.pack()
+    main_button = Button(master, text="Click me to get statrted", bg='black', fg='white', command=file_selection)
+    main_button.place(x=160, y=160)
+    master.mainloop()
+
+
+def file_selection():
+    global master2
+    master2 = Tk()
+    master2.geometry("500x300")
+    master2.title("New Window")
+
+    select_button = Button(master2, text="Browse a file",width=  18, bg='black', fg='white', command = file_procssing)
+    select_button.place(x=180, y=16)
+
+    quit_button = Button(master2, text="Quit Application ", width=18, command=exit,bg='black', fg='white')
+    quit_button.place(x=180, y=220)
+
+    master.withdraw()
+    master2.mainloop()
+
+def file_procssing():
+
+    file1 = filedialog.askopenfilename()
+    if not file1 :
+        mainloop()
+    print(file1)
+    data = get_feature_vector_from_mfcc(file1)
+
+    pickle_in = open("/home/hassan/Hassaan_Home/My_Python_Projects/Speech_Project/My_ML_Models/SVM_Model.pickle", "rb")
+    model = pickle.load(pickle_in)
+
+    prediction = model.predict([data])[0]
+
+    if prediction == 0:
+        output = "Predicted Emotion :" + "Angry"+" "
+    elif prediction == 1:
+        output = "Predicted Emotion :" + "happy"+" "
+    elif prediction == 2:
+        output = "Predicted Emotion :" + "Neutral"+"   "
+    else:
+        output = "Predicted Emotion :" + " Sad  "+"    "
+
+    msg = Message(master2, text=output)
+    msg.config(justify=CENTER, width=400, font=('times', 15, 'italic'))
+    msg.place(x=150, y=120)
+
+
+if __name__ =='__main__':
+
+    main_interface()
